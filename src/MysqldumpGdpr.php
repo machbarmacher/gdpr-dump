59279ffa78392c6f6993a4c15f90cc6b111f41ce
Axel Rutz <axel.rutz@clever-systems.net>
2018-04-11 01:10:57 +0200

    Added debug-sql option

--- src/MysqldumpGdpr.php
+++ src/MysqldumpGdpr.php
@@ -9,10 +9,17 @@ class MysqldumpGdpr extends Mysqldump {
   /** @var [string][string]string */
   protected $gdprExpressions;
 
+  /** @var bool */
+  protected $debugSql;
+
   public function __construct($dsn = '', $user = '', $pass = '', array $dumpSettings = array(), array $pdoSettings = array()) {
     if (array_key_exists('gdpr-expressions', $dumpSettings)) {
       $this->gdprExpressions = $dumpSettings['gdpr-expressions'];
       unset($dumpSettings['gdpr-expressions']);
+    }
+    if (array_key_exists('debug-sql', $dumpSettings)) {
+      $this->debugSql = $dumpSettings['debug-sql'];
+      unset($dumpSettings['debug-sql']);
     }
     parent::__construct($dsn, $user, $pass, $dumpSettings, $pdoSettings);
   }
@@ -24,6 +31,9 @@ class MysqldumpGdpr extends Mysqldump {
       if (!empty($this->gdprExpressions[$tableName][$columnName])) {
         $columnStmt[$i] = $this->gdprExpressions[$tableName][$columnName];
       }
+    }
+    if ($this->debugSql) {
+      print "/* SELECT " . implode(",", $columnStmt) . " FROM `$tableName` */";
     }
     return $columnStmt;
   }
