4bb81d31ee309ddd14de93cc2fa879009dfc067f
Blaize Kaye <blaize.kaye@gmail.com>
2018-05-28 06:46:31 +1200

    removed idea, fixed testjson

--- tests/ColumnTransformerTest.php
+++ tests/ColumnTransformerTest.php
@@ -5,7 +5,7 @@ use \PHPUnit\Framework\TestCase;
 
 class ColumnTransformerTest extends TestCase
 {
-    private $jsonData = '{"users_field_data":{"name":"uid","mail":"uid","init":"uid","pass":{"transformer":"faker", "replacement":""}}}';
+    private $jsonData = '{"users_field_data":{"name":"uid","mail":"uid","init":"uid","pass":{"transformer":"faker", "formatter":"password"}}}';
 
     public function testCreatingOfNewExpressionStatement()
     {
