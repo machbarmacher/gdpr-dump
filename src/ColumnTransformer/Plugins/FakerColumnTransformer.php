ca76d2d1d465f7bf4b9168f505a1120b7ea2d543
Bozhidar Hristov <bozhidar.hristov@stanga.net>
2019-08-12 16:38:27 +0300

    Allow registration of custom transformers and pass whole event to getValue()

--- src/ColumnTransformer/Plugins/FakerColumnTransformer.php
+++ src/ColumnTransformer/Plugins/FakerColumnTransformer.php
@@ -3,8 +3,8 @@
 namespace machbarmacher\GdprDump\ColumnTransformer\Plugins;
 
 use Faker\Factory;
-use Faker\Provider\Base;
 use machbarmacher\GdprDump\ColumnTransformer\ColumnTransformer;
+use machbarmacher\GdprDump\ColumnTransformer\ColumnTransformEvent;
 
 class FakerColumnTransformer extends ColumnTransformer
 {
@@ -30,21 +30,21 @@ class FakerColumnTransformer extends ColumnTransformer
     {
         if (!isset(self::$generator)) {
             self::$generator = Factory::create();
-            foreach(self::$generator->getProviders() as $provider)
-            {
+            foreach (self::$generator->getProviders() as $provider) {
                 $clazz = new \ReflectionClass($provider);
                 $methods = $clazz->getMethods(\ReflectionMethod::IS_PUBLIC);
-                foreach($methods as $m)
-                {
-                    if(strpos($m->name, '__') === 0) continue;
+                foreach ($methods as $m) {
+                    if (strpos($m->name, '__') === 0) {
+                        continue;
+                    }
                     self::$formatterTansformerMap[$m->name] = $m->name;
                 }
             }
         }
     }
 
-    public function getValue($expression)
+    public function getValue(ColumnTransformEvent $event)
     {
-        return self::$generator->format(self::$formatterTansformerMap[$expression['formatter']]);
+        return self::$generator->format(self::$formatterTansformerMap[$event->getExpression()['formatter']]);
     }
 }
